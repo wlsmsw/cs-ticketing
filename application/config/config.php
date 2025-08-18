38412d7271f3583f9a15e9e484479dafaeaa3c9d
Sherwin Nino Macalintal <snmacalintal@ph.esl-asia.com>
2025-08-18 17:53:37 +0800

    ISB-1344

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '_SITE_URL_';
+$config['base_url'] = _SITE_URL_;
 
 /*
 |--------------------------------------------------------------------------
